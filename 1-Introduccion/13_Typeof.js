bf4faa26825f87888d89e90e12cff6d7dc0f6a67
Alfred <arista21dominguez@gmail.com>
2024-11-30 18:26:22 -0600

    Typeof

---
+++ 1-Introduccion/13_Typeof.js
@@ -0,0 +1,16 @@
+console.log("5" + "7");
+console.log(typeof(32.4));
+console.log(typeof(true));
+
+let x = "55";
+let y = Number(x);
+console.log(typeof y);
+
+let v = "55f";
+let z = Number(v);
+console.log(typeof z);
+console.log(z);
+
+let a = true;
+let b = String(a);
+console.log(typeof b);
