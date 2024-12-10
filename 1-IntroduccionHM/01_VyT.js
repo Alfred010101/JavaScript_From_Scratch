419f1d3af9c6c8f4cf4273d2a0d2888e8ccaba8c
Alfred <arista21dominguez@gmail.com>
2024-12-09 21:37:03 -0600

    Variables y Tipos de datos primitivos

---
+++ 1-IntroduccionHM/01_VyT.js
@@ -0,0 +1,17 @@
+//Variables y Tipos 
+
+let numero = 1
+let texto = "Nombre"
+let booleano = false
+let nulo = null
+let noDefinido
+
+console.log(numero)
+console.log(texto)
+console.log(booleano)
+
+numero = "Uno"
+console.log(numero)
+
+const constante = "Texto sin cambiar"
+console.log(constante)
