6d22e50f3e0c4b3b6ce53619569d4a251720bbcb
Alfred <arista21dominguez@gmail.com>
2024-12-10 12:02:57 -0600

    Operadores logicos

---
+++ 1-IntroduccionHM/03_Operadores.js
@@ -0,0 +1,65 @@
+console.log("Operadores aritmeticos");
+console.log(4 + 5);
+console.log(9 - 2);
+console.log(2 * 2);
+console.log(20 / 5);
+console.log(10 % 4);
+console.log(2 ** 4);
+
+console.log("\nOperadores relacionales");
+console.log(4 > 5);
+console.log(9 < 2);
+console.log(2 >= 2);
+console.log(20 <= 5);
+console.log(10 != 4);
+
+console.log("\nOperadores logicos");
+console.log(false && true);
+console.log(true || false);
+console.log(!true);
+
+console.log("\nOperadores de asignacion");
+let asignacion = "Texto"
+let suma = "5"
+let resta = "21", multi = "3", div = 8, modulo = 33, pow = "3"
+
+suma += "6"
+resta -= "8"
+multi *= "2"
+div /= 2
+modulo %=3
+pow **="3"
+
+
+console.log(asignacion)
+console.log(suma)
+console.log(resta)
+console.log(multi)
+console.log(div)
+console.log(modulo)
+console.log(pow)
+
+console.log("Tipos de datos")
+console.log(typeof asignacion)
+console.log(typeof suma)
+console.log(typeof resta)
+console.log(typeof multi)
+console.log(typeof div)
+console.log(typeof modulo)
+console.log(typeof pow)
+
+console.log("Operadores unarios")
+console.log(++suma)
+console.log(suma++)
+console.log(suma)
+
+console.log(--resta)
+console.log(resta--)
+console.log(resta)
+
+console.log(typeof suma)
+console.log(typeof resta)
+
+console.log("Operadores bitwise")
+console.log(2  |  7)
+console.log(2  &  7)
