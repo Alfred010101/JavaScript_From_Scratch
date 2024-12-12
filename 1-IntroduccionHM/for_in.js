889ea7823a71af4691375fc6d1dc8e1bbd103066
Alfred <arista21dominguez@gmail.com>
2024-12-12 17:01:51 -0600

    for in - for of

---
+++ 1-IntroduccionHM/for_in.js
@@ -0,0 +1,9 @@
+user = {
+    nameUser : 'Juan',
+    edad : 21,
+    color : 'red'
+}
+
+for (const key in user) {
+    console.log(user[key])
+}
---
+++ 1-IntroduccionHM/for_of.js
@@ -0,0 +1,5 @@
+let animales = ['perro', 'gato']
+
+for (const element of animales) {
+    console.log(element)
+}
